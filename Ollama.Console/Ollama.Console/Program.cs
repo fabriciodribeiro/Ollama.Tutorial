e9ad62178ace98ee1bddfc160099da529003ec12
Fabricio Ribeiro <fabriciodribeiro@hotmail.com>
2026-03-09 21:56:32 +0000

    feat: fazer com que o modelo reconhecesse uma imagem de um recibo

--- Ollama.Console/Ollama.Console/Program.cs
+++ Ollama.Console/Ollama.Console/Program.cs
@@ -5,13 +5,22 @@ using OllamaSharp;
 
 var builder = Host.CreateApplicationBuilder();
 
-builder.Services.AddChatClient(new OllamaApiClient(new Uri("http://localhost:11434"), "llama3.2:1b"));
+builder.Services.AddChatClient(new OllamaApiClient(new Uri("http://localhost:11434"), "gemma3:latest"));
 
 var app = builder.Build();
-
 var chatClient = app.Services.GetRequiredService<IChatClient>();
-var message = new ChatMessage(ChatRole.User, "Qual é a capital da França?");
-var response = await chatClient.GetResponseAsync([message]);
 
-Console.Write(response);
-Console.ReadLine();
+
+//Sample simple chat
+var messageChat = new ChatMessage(ChatRole.User, "Qual é a capital de Angola?");
+var responseChat = await chatClient.GetResponseAsync([messageChat]);
+Console.WriteLine("Simple chat response: "+ responseChat.Text);
+
+//Sample with image - Analyse the receipt
+// var messageReceiptAnalysis = new ChatMessage(ChatRole.User, "What's in the image?");
+// messageReceiptAnalysis.Contents.Add(new DataContent( File.ReadAllBytes("Receipt/reciept1.png"), "image/png"));
+// var receiptAnalysisResponse = await chatClient.GetResponseAsync([messageReceiptAnalysis]);
+
+// Console.WriteLine("Analyse the receipt: "+ receiptAnalysisResponse.Text);
+
+//Console.ReadLine();
